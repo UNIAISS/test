e0376a52b1286addfa94d62641886b5459b7d13c
UNIAISS <97916447+UNIAISS@users.noreply.github.com>
2022-03-16 15:54:50 +0800

    testlab

---
+++ testlab.js
@@ -0,0 +1,9 @@
+>>>this is a test project
+>>>when utc+8=6:00:00;
+>>>labserver=Y;
+>>>labdatabases=Y;
+>>>Setting IP Adress::;
+>>>start//::
+>>>IP icde=192.168.0.1
+>>>ita=all ipt ~.255;//
+>>>finish;//
